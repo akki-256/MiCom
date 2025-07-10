ecd0025417466742a2def93fcd1264200aa50e67
k23022 <k23022kk@aitech.ac.jp>
2025-07-10 11:56:47 +0900

    delay時間を調整

--- senser/senser.ino
+++ senser/senser.ino
@@ -18,16 +18,17 @@ void setup() {
 }
 
 void loop() {
+  
   // センサから値を取得
   int x = abs(analogRead(xPin) - 500);
   int y = abs(analogRead(yPin) - 500);
   int z = abs(analogRead(zPin) - 500);
   int strength = x + y + z - 200;
 
-  unsigned long now = millis();
+  // unsigned long now = millis();
 
-  // クールダウン期間を設けて同じ振りを連続検出しないようにする
-  if (now - lastHitTime < 750) return;
+  // // クールダウン期間を設けて同じ振りを連続検出しないようにする
+  // if (now - lastHitTime < 750) return;
 
   // 振りを検知（強さを判定）
   String level = "";
@@ -46,9 +47,9 @@ void loop() {
     digitalWrite(sendPin, HIGH); // 割り込み信号を送信
     delay(50);                   // 相手側に伝わるように少し保持
     digitalWrite(sendPin, LOW);  // 割り込み信号を終了
-
-    lastHitTime = now;           // 最終検出時間を更新
+    delay(700);
+    // lastHitTime = now;           // 最終検出時間を更新
   }
 
-  delay(10);  // 通信負荷軽減
+  // delay(10);  // 通信負荷軽減
 }
