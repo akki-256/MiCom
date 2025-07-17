0ced9e7c4115bba6209b912438a0042940ae6664
akki-256 <168257172+akki-256@users.noreply.github.com>
2025-07-17 11:31:53 +0900

    mainの大枠
    考慮もれはおそらく多いと思います
    また．ユーザが振った時にアニメーションを再生するロジックは未実装です

--- LED/main.ino
+++ LED/main.ino
@@ -1,7 +1,11 @@
 int interruptPin = 2;  //割り込み信号用ピン　　D2,D3のみ
 
-volatile unsigned interruptTime = 0;  //割り込みが発生したかどうかを格納する　loop内で初期化予定
-bool isAnimating = false;
+volatile unsigned interruptTime = 0; //割り込み発生時の時間
+volatile unsigned playerMovingTime = 0;  //相手のターンでの割り込み発生時．音の再生，判定で使用
+bool isAnimating = false;               //アニメーションの動き
+int playerStrong = -1;  //0:弱,1:中,2:強
+int cpuStrong = -1;
+bool playerTurn;//自分のターン
 
 void setup() {
   //割り込み設定(割り込み発火監視ピン，割り込み時走らせる関数，モード)
@@ -13,19 +17,29 @@ void setup() {
     lc.clearDisplay(i);
   }
   // manを左上、man_in_the_mirrorを右下に表示
-  drawPatch(6, 0, man);             // 左上（上段）
+  drawPatch(6, 0, man);                 // 左上（上段）
   drawPatch(6, 24, man_in_the_mirror);  // 右下（下段）
+  while (interruptTime <= 0) {
+  }
+  playerStrong = 1;
+  playerTurn = true;
 }
 
 void loop() {
-  if (interruptFLG && Serial.available()) {
-    interruptFLG = false;
-    judge(Serial.read());
+  if (playerTurn) {
+    //プレイヤーが打った後
+    playSound(playerStrong, playerMovingTime);
+    cpuStrong = couJudge();  //適当なタイミングでplayerTurnをfalseにする
+  } else {
+    //プレイヤーがこれから打つ
+    playSound(cpuStrong, cpuTime);
+    playerStrong = judge(interruptTime);  //この関数の副作用でplayerTurnをtrueにする
   }
-  BollaAnime();
+
   showMatrix();
 }
 
 void onInterrupt() {
   interruptTime = millis();
+  if (!playerTurn) playerMovingTime = interruptTime;
 }
